b7d3d3623e1b430a54676fa53c846026352a46ac
Kristof Dewilde <code@qrivi.dev>
2021-07-15 19:50:26 +0200

    Reformatted React objects for better style consistency

--- src/Snake.js
+++ src/Snake.js
@@ -2,25 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 // component
-const Snake = ({ snake }) => {
-    const snakeParts = snake.map(
-        part => <div key={part.x + "_" + part.y} className="snake" style={{ left: part.x * 10 + 'px', top: part.y * 10 + 'px' }}></div>
-    )
-    return (
-        <div >
-            {snakeParts}
-        </div>
-    )
-}
+const Snake = ({ snake }) => (
+    <div id="snake">
+        {snake.map(
+            part => <div key={part.x + "_" + part.y} className="snakepart" style={{ left: part.x * 10 + 'px', top: part.y * 10 + 'px' }}></div>
+        )}
+    </div>
+)
 
 // StateToProps
-const mapStateToProps = (state) => {
-    return {
-        snake: state.snake
-    }
-}
+const mapStateToProps = (state) => ({
+    snake: state.snake
+})
 
 // connect
 export default connect(mapStateToProps)(Snake)
-
-
