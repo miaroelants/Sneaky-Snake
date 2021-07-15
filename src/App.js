056ea69e5f7b44fdb1f3a938e5fe53433bcc5179
Kristof Dewilde <code@qrivi.dev>
2021-07-15 19:49:38 +0200

    Replaced GameOver.js with Overlay.js, and bit of restyling

--- src/App.js
+++ src/App.js
@@ -2,43 +2,28 @@ import React from 'react';
 import './App.css';
 import Snake from './Snake'
 import Apple from './Apple'
-import Level from './Level'
 import Score from './Score'
-import GameOver from './GameOver'
+import Overlay from './Overlay'
 import { connect } from 'react-redux'
 
-const App = ({gameOver}) => {
-    if (gameOver === false ) {
-      return ( 
-          <div id="App">
-            <div id="meta">
-              <Score />
-              <Level />
-            </div>
-            <div id="field">
-              <Apple />
-              <Snake />
-            </div>          
-          </div>
-      );
-    }
-    else {
-      return (
-        <div id="App">
-          <GameOver />        
-        </div>
-      );
-    }
-  }
-
+const App = () => (
+  <div id="App">
+    <h1>Snaek</h1>
+    <div id="field">
+      <Apple />
+      <Snake />
+      <Overlay />
+    </div>
+    <div id="meta">
+      <Score />
+    </div>
+  </div>
+)
 
 // StateToProps
-const mapStateToProps = (state) => {
-  return {
-    gameOver: state.gameOver
-  }
-}
+const mapStateToProps = (state) => ({
+  gameState: state.gameState
+})
 
 // connect
 export default connect(mapStateToProps)(App)
-
